ad3095055609062ffdd7525610757b060047427f
mchoi <mchoi@g.hmc.edu>
2015-11-14 04:59:24 -0800

    Stuff

--- Hackathon/Assets/Scripts/CookUI.cs
+++ Hackathon/Assets/Scripts/CookUI.cs
@@ -3,7 +3,16 @@ using System.Collections;
 
 public class CookUI : MonoBehaviour {
 
-    GameObject CakeButton;
-    
-    CakeButton.onClick()
+    public void Win()
+    {
+        int sugar = GameManager.instance.playerInventory["Sugar"];
+        int strawberry = GameManager.instance.playerInventory["Strawberry"];
+        if (sugar >= 2 && strawberry >= 2)
+        {
+            GameObject cake = GameObject.Find("RawImage");
+            cake.SetActive(true);
+        }
+        
+        // do nothing
+    }
 }
