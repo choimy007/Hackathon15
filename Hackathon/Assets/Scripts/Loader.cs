ed49a1c0f5e2afdbe174120962f5dfb9108b0fff
akubota95 <akubota@g.hmc.edu>
2015-11-13 20:09:52 -0800

    Added scripts to Unity

---
+++ Hackathon/Assets/Scripts/Loader.cs
@@ -0,0 +1,15 @@
+﻿// Checks if gameManager has been instantiated.  If not, it instantiates one from from our prefabs
+
+using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+	public GameObject gameManager;
+
+	// Use this for initialization
+	void Awake () {
+		if (GameManager.instance == null) {
+			Instantiate (gameManager);
+		}
+	}
+}
