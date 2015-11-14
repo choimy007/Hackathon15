5b882b514c3fee461388a272bb44babf19d7a979
akubota95 <akubota@g.hmc.edu>
2015-11-14 05:25:10 -0800

    Table flip

--- Hackathon/Assets/Scripts/CookUI.cs
+++ Hackathon/Assets/Scripts/CookUI.cs
@@ -7,8 +7,10 @@ public class CookUI : MonoBehaviour {
     {
         int sugar = GameManager.instance.playerInventory["Sugar"];
         int strawberry = GameManager.instance.playerInventory["Strawberry"];
-        if (sugar >= 2 && strawberry >= 2)
+        if (sugar >= 1 && strawberry >= 1)
         {
+        	Debug.Log("sugar is " + sugar);
+        	Debug.Log("strawberry is " + strawberry);
             GameObject cake = GameObject.Find("RawImage");
             cake.SetActive(true);
         }
